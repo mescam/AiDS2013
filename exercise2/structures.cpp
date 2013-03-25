69789ad866574eb3f20f572c7f0fb5a61df6dda3
Marcin Zaremba <marcin.m.zaremba@student.put.poznan.pl>
2013-03-25 17:24:31 +0000

    list impl finished + unique number generator

--- exercise2/structures.cpp
+++ exercise2/structures.cpp
@@ -18,85 +18,155 @@
  */
 
 #include <iostream>
-#include <cstdio>
+#include <climits>
+#include <cstdlib>
+#include <ctime>
 
-// Dynamic list implementation
-namespace List {
-  struct ListElement {
-    int key;
-    ListElement *next;
-  };
+/*
+ * Dynamic list implementation
+ *
+ */
 
-  typedef ListElement* PListElement;
+struct ListElement {
+  int key;
+  ListElement *next;
+};
 
-  PListElement initElement(int);
-  void insertElement(PListElement &, int);
-  PListElement searchList(PListElement &, int);
-  void printList(PListElement &);
-  void freeList(PListElement &);  
+typedef ListElement* PListElement;
 
-  PListElement initElement(int key) {
-    PListElement el = new ListElement;
-    el->key = key;
-    el->next = NULL;
+PListElement initElement(int);
+void insertElement(PListElement &, int);
+PListElement searchList(PListElement &, int);
+void printList(PListElement &);
+void freeList(PListElement &);  
 
-    return el;
-  }
+PListElement initList() {
+  /*
+    why is that thing?
+    there're our guards, inserting with 'em is just heaven,
+    without - hell. (so much ifs needed then)
+  */
+  PListElement el = initElement(-INT_MAX);
+  el->next = initElement(INT_MAX);
 
-  void freeList(PListElement &head) {
-    PListElement el, toDel;
+  return el;
+}
 
-    el = head;
+PListElement initElement(int key) {
+  PListElement el = new ListElement; 
+  el->key = key;
+  el->next = NULL;
 
-    while(el != NULL) {
-      toDel = el;
-      el = el->next;
+  return el;
+}
+
+void destroyElement(PListElement &head, int key) {
+  PListElement el = head;
+
+  while(el->key != INT_MAX) {
+    if(el->next->key == key) {
+      PListElement toDel = el->next;
+      el->next = el->next->next;
       delete toDel;
+      break;
     }
-  }
 
-// version to just test,
-// self-sorting list algorithm will be applied
-  void insertElement(PListElement &head, int key) {
-    PListElement el = initElement(key);
-
-    if(head != NULL) 
-      el->next = head;
-    head = el;
-  }
-
-  PListElement searchList(PListElement &head, int key) {
-    PListElement el;
-
-    for(el = head; el != NULL && el->key != key; el = el->next);
-
-    return el;
-  }
-
-  void printList(PListElement &head) {
-    PListElement el = head;
-
-    while(el != NULL) {
-      std::cout << el->key << " ";
-      el = el->next;
-    }
+    el = el->next;
   }
 }
 
-int main() {
-  using namespace List;
+void freeList(PListElement &head) {
+  PListElement el, toDel;
+
+  el = head;
+
+  while(el != NULL) {
+    toDel = el;
+    el = el->next;
+    delete toDel;
+  }
+}
+
+void insertElement(PListElement &head, int key) {
+  PListElement toIns = initElement(key);
+  PListElement el;
+
+  for(el = head; key > el->next->key; el = el->next);
+  toIns->next = el->next;
+  el->next = toIns;
+}
+
+PListElement searchList(PListElement &head, int key) {
+  PListElement el;
+
+  for(el = head; el->key != INT_MAX; el = el->next)
+    if(el->key == key)
+      break;
+
+  return el->key == INT_MAX ? NULL : el;
+}
+
+void printList(PListElement &head) {
+  PListElement el = head->next;
+
+  while(el->key != INT_MAX) {
+    std::cout << el->key << " ";
+    el = el->next;
+  }
+}
+
+int* generateData(const int n) {
+  int *a = new int[n];
+  int c = 0;
+//  int uniqCounter = 0; //for fun :D
   
-  PListElement head = NULL;
+  do {
+    int number = rand() % (4*n);
 
-  insertElement(head, 12);
-  insertElement(head, 20);
-  insertElement(head, 2312);
+    bool isUnique = true;
+    for(int i = 0; i < c; i++)
+      if(a[i] == number) {
+        isUnique = false;
+        //      uniqCounter++;
+        break;
+      }
 
-  PListElement se = searchList(head, 231);
+    if(isUnique)
+      a[c++] = number;
+  }
+  while(c < n);
 
-  printList(head);
-  freeList(head);
+//  std::cout << "Not unique: " << uniqCounter << std::endl;
+
+  return a;
+}
+
+void printArray(int a[], int size) {
+  for(int i = 0; i < size; i++)
+    std::cout << a[i] << " ";
+}
+
+int main() {
+
+  srand(time(NULL));
+  PListElement l = initList();
+
+  insertElement(l, 32);
+  insertElement(l, 15);
+  insertElement(l, 8);
+  destroyElement(l, 15);
+  insertElement(l, 12321);
+
+  printList(l);
+  std::cout << "\n\n";
+  int *a = generateData(500);
+  printArray(a, 500);
+  freeList(l);
+  
   return 0;
 }
 
 /* vim: set ts=2 sw=2 tw=0 et :*/
+
+
+
